ca41dfa5798f051bb182bf48ede3828c7c71091c
rmamchyk <roman.mamchyk@gmail.com>
2019-08-18 14:33:54 +0300

    Added animation to Hero and Features sections

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  new WOW().init();
+
+  $('.js-wp-2').waypoint(function(direction) {
+    $('.js-wp-2').addClass('animated slideInUp');
+  }, {
+    offset: '70%'
+  });
+
+  $('.iphone-btn').delay(1700).animate({ bottom: '+=-3' }, 300);
+  $('.iphone-btn').delay(300).animate({ top: '+=-3' }, 100);
+
+  $('.js-wp-3').waypoint(function(direction) {
+    $('.js-wp-3').addClass('animated fadeIn');
+  }, {
+    offset: '70%'
+  });
+
+});
